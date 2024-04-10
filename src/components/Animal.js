3e343c23fdc638cec32dd2e6914303a0070b1fe7
Feline Huhn <feline.huhn@gmx.de>
2024-04-10 10:18:37 +0200

    initial commit

---
+++ src/components/Animal.js
@@ -0,0 +1,20 @@
+import Button from "./Button";
+import AnimalEmoji from "./AnimalEmoji";
+
+export default function Animal({ emoji, name, description }) {
+  return (
+    <>
+      <h3>
+        <AnimalEmoji>
+          {emoji}
+          {name}
+        </AnimalEmoji>{" "}
+        {name}
+      </h3>
+      {description}
+      <Button>
+        Adopt <strong>{name}</strong>
+      </Button>
+    </>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+const rootElement = document.querySelector("#root");
+const root = createRoot(rootElement);
+
+root.render(
+  <>
+    <App />
+  </>
+);
